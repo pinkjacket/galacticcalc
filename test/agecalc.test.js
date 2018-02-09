b0b2b31524eda1e24af806d4ce9824a84fa2b439
pinkjacket <33609459+pinkjacket@users.noreply.github.com>
2018-02-09 14:39:19 -0800

    test to return remaining years now works for all planets

--- test/agecalc.test.js
+++ test/agecalc.test.js
@@ -24,7 +24,7 @@ describe ("AgeCalc", function() {
       let firstDate = new Date(1987, 9, 2);
       let secondDate = new Date(2018, 1, 9);
       let mercAge = AgeCalc.compareTwoDates(firstDate, secondDate);
-      assert(AgeCalc.spaceYears(mercAge, "mercury") > 126 && AgeCalc.spaceYears(mercAge, "mercury") < 127); //I got my age on other planets off an existing website and I'm just going to test that they all come out between the two whole numbers they should be between because the decimals are huge
+      assert(AgeCalc.spaceYears(mercAge, "mercury") > 126 && AgeCalc.spaceYears(mercAge, "mercury") < 127); //I got my age on other planets off an existing website and I'm just going to test that they all come out roughly where they need to be, otherwise mocha just complains that it wanted something true but it's getting something false
     });
   });
 
@@ -62,7 +62,50 @@ describe ("AgeCalc", function() {
       let userAge = AgeCalc.compareTwoDates(firstDate, secondDate);
       let lifeExpectancy = 80;
       assert(AgeCalc.remainingYears(userAge, "earth", lifeExpectancy) > 49 && AgeCalc.remainingYears(userAge, "earth", lifeExpectancy) < 50);
+      // assert(AgeCalc.remainingYears(userAge, "mercury", lifeExpectancy) > 210 && AgeCalc.remainingYears(userAge, "mercury", lifeExpectancy) < 230);
+      // assert(AgeCalc.remainingYears(userAge, "venus", lifeExpectancy) > 49 && AgeCalc.remainingYears(userAge, "venus", lifeExpectancy) < 50);
+      // assert(AgeCalc.remainingYears(userAge, "mars", lifeExpectancy) > 49 && AgeCalc.remainingYears(userAge, "mars", lifeExpectancy) < 50);
+      // assert(AgeCalc.remainingYears(userAge, "jupiter", lifeExpectancy) > 49 && AgeCalc.remainingYears(userAge, "jupiter", lifeExpectancy) < 50);
     })
   })
 
+  describe("remainingYears", function() {
+    it("should compare the user's age and life expectancy and return the difference on mercury", function(){
+      let firstDate = new Date(1987, 9, 2);
+      let secondDate = new Date(2018, 1, 9);
+      let userAge = AgeCalc.compareTwoDates(firstDate, secondDate);
+      let lifeExpectancy = 336; //I'm just using 80 for now since that's roughly the average on earth, this is the equivalent of 80 on the other planets. took me a bit to realize this was why all the non-earth tests were failing
+      assert(AgeCalc.remainingYears(userAge, "mercury", lifeExpectancy) > 209 && AgeCalc.remainingYears(userAge, "mercury", lifeExpectancy) < 211);
+    })
+  })
+
+  describe("remainingYears", function() {
+    it("should compare the user's age and life expectancy and return the difference on venus", function(){
+      let firstDate = new Date(1987, 9, 2);
+      let secondDate = new Date(2018, 1, 9);
+      let userAge = AgeCalc.compareTwoDates(firstDate, secondDate);
+      let lifeExpectancy = 131;
+      assert(AgeCalc.remainingYears(userAge, "venus", lifeExpectancy) > 81 && AgeCalc.remainingYears(userAge, "venus", lifeExpectancy) < 83);
+    })
+  })
+
+  describe("remainingYears", function() {
+    it("should compare the user's age and life expectancy and return the difference on mars", function(){
+      let firstDate = new Date(1987, 9, 2);
+      let secondDate = new Date(2018, 1, 9);
+      let userAge = AgeCalc.compareTwoDates(firstDate, secondDate);
+      let lifeExpectancy = 43;
+      assert(AgeCalc.remainingYears(userAge, "mars", lifeExpectancy) > 26 && AgeCalc.remainingYears(userAge, "mars", lifeExpectancy) < 28);
+    })
+  })
+
+  describe("remainingYears", function() {
+    it("should compare the user's age and life expectancy and return the difference on jupiter", function(){
+      let firstDate = new Date(1987, 9, 2);
+      let secondDate = new Date(2018, 1, 9);
+      let userAge = AgeCalc.compareTwoDates(firstDate, secondDate);
+      let lifeExpectancy = 7;
+      assert(AgeCalc.remainingYears(userAge, "jupiter", lifeExpectancy) > 3 && AgeCalc.remainingYears(userAge, "jupiter", lifeExpectancy) < 5);
+    })
+  })
 })
